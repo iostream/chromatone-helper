4ce342cf23738266ecd478e0290cce09e886f540
r/iostream <you@example.com>
2018-09-08 13:16:38 +0200

    finalize first version of build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+indexFileName="chromatone-combined-index.html"
+
 mkdir -p build/chromatone
 
 rm -fr build/chromatone/*
@@ -7,3 +9,31 @@ rm -fr build/chromatone/*
 cp chromatone-index.html build/chromatone
 cp -r src build/chromatone
 cp -r resources build/chromatone
+
+cd build
+# zip -r chromatone.zip chromatone
+
+cd chromatone
+cp chromatone-index.html "$indexFileName"
+
+replaceScriptFile() {
+  indexFilePath=$1
+  filePath=$2
+  filePathEscaped=$(printf '%s' "$filePath" | sed -e 's@/@\\/@g')
+  sed -e "/$filePathEscaped/ {" -e "a <script>" -e "r $filePath" -e "a </script>" -e 'd' -e '}' -i $indexFilePath
+}
+
+replaceStyleFile() {
+  indexFilePath=$1
+  filePath=$2
+  filePathEscaped=$(printf '%s' "$filePath" | sed -e 's@/@\\/@g')
+  sed -e "/$filePathEscaped/ {" -e "a <style>" -e "r $filePath" -e "a </style>" -e 'd' -e '}' -i $indexFilePath
+}
+
+replaceStyleFile "$indexFileName" "resources/style.css"
+
+replaceScriptFile "$indexFileName" "src/theory.js"
+replaceScriptFile "$indexFileName" "src/fingering.js"
+replaceScriptFile "$indexFileName" "src/progression.js"
+replaceScriptFile "$indexFileName" "src/gui.js"
+replaceScriptFile "$indexFileName" "resources/progressions.json"
