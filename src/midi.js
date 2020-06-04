f7c242c4bd9cfceab5b65be542159ba9519ec55b
Tommy Helm <746114+iostream@users.noreply.github.com>
2020-06-04 20:45:11 +0200

    Make MIDI export work again

--- src/midi.js
+++ src/midi.js
@@ -12,7 +12,7 @@ function buildScalesDescription(scales) {
  *
  * @return MidiWriter object
  */
-lib.createMidi = function(events, chords, scales, generatorUrl) {
+lib.createMidi = function(chordEvents, chords, scales, generatorUrl) {
   var MidiWriter = require("midi-writer-js");
 
   // Start with a new track
@@ -22,25 +22,27 @@ lib.createMidi = function(events, chords, scales, generatorUrl) {
   track.addTrackName(buildScalesDescription(scales) + " Generated via: " + generatorUrl);
 
   var restingTime = 0;
-  events.forEach(function(event) {
-    // Tn : where n is an explicit number of ticks (T128 = 1 beat)
-    var eventDuration = "T" + event.getLengthInQN() * 128;
-    if (event.isRest()) {
-      restingTime += event.getLengthInQN();
-    } else {
-      var pitches = event.getPitches().map(function(note) {
-        return note.getPosition();
-      });
-      var noteData = {pitch: pitches, duration: eventDuration};
-      if (restingTime > 0) {
-        // Tn : where n is an explicit number of ticks (T128 = 1 beat)
-        noteData.wait = "T" + restingTime * 128;
-        restingTime = 0;
+  chordEvents.forEach(function(events) {
+    events.forEach(function(event) {
+      // Tn : where n is an explicit number of ticks (T128 = 1 beat)
+      var eventDuration = "T" + event.getLengthInQN() * 128;
+      if (event.isRest()) {
+        restingTime += event.getLengthInQN();
+      } else {
+        var pitches = event.getPitches().map(function(note) {
+          return note.getPosition();
+        });
+        var noteData = {pitch: pitches, duration: eventDuration};
+        if (restingTime > 0) {
+          // Tn : where n is an explicit number of ticks (T128 = 1 beat)
+          noteData.wait = "T" + restingTime * 128;
+          restingTime = 0;
+        }
+        track.addEvent(new MidiWriter.NoteEvent(noteData));
       }
-      track.addEvent(new MidiWriter.NoteEvent(noteData));
-    }
-    // TODO can we bring back chord names in MIDI export?
-    // track.addMarker(chord.getName());
+      // TODO can we bring back chord names in MIDI export?
+      // track.addMarker(chord.getName());
+    });
   });
 
   var writer = new MidiWriter.Writer([track]);
